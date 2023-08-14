bf23a4af97d48b8f76c1301772662d6e93d5656b
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-14 13:47:15 +0200

    created special files to keep landing basic info, and images

---
+++ src/utils/data-settings.js
@@ -0,0 +1,6 @@
+const landingSettings = {
+  title: "ABC PRAWIDŁOWEJ PIELĘGNACJI DOMOWEJ",
+  date: "20.04.2024",
+};
+
+export default landingSettings;
---
+++ src/utils/images.js
@@ -0,0 +1,3 @@
+import logo from "../assets/LOGO.webp";
+
+export default logo;
