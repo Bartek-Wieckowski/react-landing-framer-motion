da3ef38de71928b243e3561d5ceba2636bea2457
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-14 13:48:25 +0200

    used Header component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Header from "./components/Header";
+import landingSettings from "./utils/data-settings";
 function App() {
   return (
-    <div className="container">
-      <div>test</div>
+    <div className="app">
+      <Header webinarTitle={landingSettings.title} webinarDate={landingSettings.date} />
+      <div className="container">
+        <div>test</div>
+      </div>
     </div>
   );
 }
