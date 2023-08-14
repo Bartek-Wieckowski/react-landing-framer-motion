69e42d734d38662c41897f70dac0d376df34a85a
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-14 15:13:23 +0200

    added text variables to Home page

--- src/utils/data-settings.js
+++ src/utils/data-settings.js
@@ -3,4 +3,23 @@ const landingSettings = {
   date: "20.04.2024",
 };
 
-export default landingSettings;
+const homeTitle =
+  "Czy zgodzisz się, że misja to odpowiedzialne zadanie do spełnienia?\n To także drogowskaz, który wyznacza kierunek działań.";
+const homeTxt1 =
+  "Naszą misją jest tworzenie miejsca, dzięki któremu polepszysz swoje zdrowie a poprawa wyglądu będzie tylko przyjemnym skutkiem ubocznym!";
+const homeTxt2 =
+  'Wierzymy, że skóra to "lustro organizmu". Jeśli pojawia się na niej trądzik nadwrażliwość, zaczerwienienie, czy zmarszczki nieadekwatne do wieku, to jest to sygnał od ciała, że coś należy zmienić.';
+const homeTxt3 =
+  "W Misji Perfekcji pomagamy naszym Klientom dotrzeć do źródła ich problemów z cerą przez wnikliwe konsultacje i badania laboratoryjne. Układamy plany suplementacji, pielęgnacji domowej i zabiegów w gabinecie, aby jak najszybciej dotrzeć do wspólnego celu, którym jest Twoja zdrowa cera.";
+const homeTxt4 =
+  "Dążymy nie tylko do perfekcyjnego wykonywania zabiegów, ale także do perfekcyjnej obsługi, po to, abyś poczuła się zaopiekowania, bezpieczna i pewna efektów.";
+const homeTxt5 =
+  "Nasz zespół tworzą ludzie z niepohamowaną potrzebą rozwoju. Uwielbiamy swoją pracę i naszych Klientów. Czujemy wspólną misję i wiemy, że razem możemy mieć realny wpływ na poprawę życia naszych Klientów.";
+const homeTxt6 = "Będziesz w najlepszych rękach!";
+
+const homeContetObj = {
+  title: [homeTitle],
+  txts: [homeTxt1, homeTxt2, homeTxt3, homeTxt4, homeTxt5, homeTxt6],
+};
+
+export { landingSettings, homeContetObj };
