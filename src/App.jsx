a9a429967a53ccdc44ef3bea644c87dee5bfad55
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-14 15:14:36 +0200

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import landingSettings from "./utils/data-settings";
+import {landingSettings, homeContetObj} from "./utils/data-settings";
+import Home from "./pages/Home";
+import Registration from "./pages/Registration";
+import About from "./pages/About";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
   return (
     <div className="app">
       <Header webinarTitle={landingSettings.title} webinarDate={landingSettings.date} />
+
       <div className="container">
-        <div>test</div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home homeContent={homeContetObj}/>}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/registration" element={<Registration />}></Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   );
