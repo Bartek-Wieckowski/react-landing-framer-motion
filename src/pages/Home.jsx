c502fda2bd9bcb4d7a113ad2469efaecc7f954fa
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-16 15:23:27 +0200

    created state, function and add logic to display right button depending on countdownExpired var

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { landingSettings } from "../utils/data-settings";
@@ -33,17 +34,29 @@ const buttonVariants = {
   },
 };
 
-
 export default function Home() {
+  const [countdownExpired, setCountdownExpired] = useState(false);
+  function handleCountdownExpired() {
+    setCountdownExpired(true);
+  }
   return (
     <motion.div className="home" variants={containerVariants} initial="hidden" animate="visible" exit="exit">
       <div className="home-wrapper">
-        <CountdownTimer targetDate={landingSettings.date}/>
-        <Link to="/about">
-          <motion.button variants={buttonVariants} whileHover="hover">
-            DOWIEDZ SIĘ WIĘCEJ...
-          </motion.button>
-        </Link>
+        <CountdownTimer targetDate={landingSettings.date} onCountdownExpired={handleCountdownExpired} />
+
+        {countdownExpired ? (
+          <a href="https://www.misjaperfekcja.pl/blog" target="_blank" rel="noopener noreferrer">
+            <motion.button className="button" variants={buttonVariants} whileHover="hover">
+              CZYTAJ NASZ BLOG!
+            </motion.button>
+          </a>
+        ) : (
+          <Link to="/about">
+            <motion.button className="button" variants={buttonVariants} whileHover="hover">
+              DOWIEDZ SIĘ WIĘCEJ...
+            </motion.button>
+          </Link>
+        )}
       </div>
     </motion.div>
   );
