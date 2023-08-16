c2d9dc8851c7aa6926546b0f8bb7f3370f515dd2
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-16 17:04:34 +0200

    added another img and used them in object

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,9 +1,17 @@
 import logo from "../assets/LOGO.webp";
 import homeMainImg from "../assets/home.jpg";
+import laptopImg from "../assets/laptop.png";
+import phoneImg from "../assets/phone.png";
+import laptopPDF from "../assets/cut-pdf.jpg";
+import phonePDF from "../assets/cut-phone-pdf.jpg";
 
 const imagePaths = {
   logo,
   homeMainImg,
+  laptopImg,
+  phoneImg,
+  laptopPDF,
+  phonePDF,
 };
 
 export default imagePaths;
