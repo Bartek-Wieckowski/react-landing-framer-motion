9bbff11f83e09690abfbd5a30c4a933e7aa2b844
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-16 17:04:09 +0200

    removed animations

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,76 +1,53 @@
-import { Link } from 'react-router-dom';
-import DescriptionWebinar from '../components/DescriptionWebinar';
-import imagePaths from '../utils/images';
-import { motion } from 'framer-motion';
+import { Link } from "react-router-dom";
+import DescriptionWebinar from "../components/DescriptionWebinar";
+import imagePaths from "../utils/images";
+import { motion } from "framer-motion";
 
 const containerVariants = {
   hidden: {
     opacity: 0,
-    x: '100vw',
+    x: "100vw",
   },
   visible: {
     opacity: 1,
     x: 0,
     transition: {
-      type: 'spring',
+      type: "spring",
       delay: 0.5,
     },
   },
   exit: {
-    x: '-100vw',
-    transition: { ease: 'easeInOut' },
+    x: "-100vw",
+    transition: { ease: "easeInOut" },
   },
 };
 
 const buttonVariants = {
   hover: {
     scale: 1.1,
-    textShadow: '0px 0px 8px rgb(255,255,255)',
-    boxShadow: '0px 0px 8px rgb(255,255,255)',
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
     transition: {
-      repeatType: 'mirror',
+      repeatType: "mirror",
       repeat: Infinity,
       duration: 0.3,
     },
   },
 };
-const nextVariants = {
-  hidden: {
-    x: '-100vw',
-  },
-  visible: {
-    x: 0,
-    transition: { type: 'spring', stiffness: 120 },
-  },
-};
+
 export default function About({ webinarContent }) {
   return (
-    <motion.div
-      className="about"
-      variants={containerVariants}
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-    >
+    <motion.div className="about" variants={containerVariants} initial="hidden" animate="visible" exit="exit">
       <div className="about-wrapper">
-        <img
-          src={imagePaths.homeMainImg}
-          alt="Misja Perfekcja w pracy"
-          className="about-main-img"
-        />
+        <img src={imagePaths.homeMainImg} alt="Misja Perfekcja w pracy" className="about-main-img" />
         <DescriptionWebinar webinarContent={webinarContent} />
-        <motion.div
-          className="next"
-          variants={nextVariants}
-          initial="hidden"
-          animate="visible"
-        >
+        <div className="next">
           <Link to="/registration">
-            <motion.button variants={buttonVariants} whileHover="hover">
+            <motion.button className="button" variants={buttonVariants} whileHover="hover">
               Dalej
             </motion.button>
           </Link>
-        </motion.div>
+        </div>
       </div>
     </motion.div>
   );
