6b8d6e28baafc7d0fa9a270a6bd7f18824d5aefb
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-17 12:05:30 +0200

    changed inputs to display, added state to focused variable, used to onBlur handleFocus function and focused attr

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,36 +1,36 @@
-const inputs = [
-  {
-    id: 1,
-    name: "Imię",
-    type: "text",
-    placeholder: "Imię",
-    errorMessage: "Wpisz conajmniej 3 znaki nie zawierające znaków specjalnych i liczb",
-    label: "Imię",
-    pattern: "^[A-Za-z]{3,16}$",
-    required: true,
-  },
-  {
-    id: 2,
-    name: "Nazwisko",
-    type: "text",
-    placeholder: "Nazwisko",
-    errorMessage: "Wpisz conajmniej 3 znaki nie zawierające znaków specjalnych i liczb",
-    label: "Nazwisko",
-    pattern: "^[A-Za-z]{3,16}$",
-    required: true,
-  },
-  {
-    id: 3,
-    name: "email",
-    type: "email",
-    placeholder: "Email",
-    errorMessage: "Wprowadź poprawy adres!",
-    label: "Email",
-    required: true,
-  },
-];
+import { useState } from "react";
 
 export default function Form() {
+  
+  const inputs = [
+    {
+      id: 1,
+      name: "Imię",
+      type: "text",
+      placeholder: "Wpisz swoje imię",
+      errorMessage: "Wpisz conajmniej 3 litery bez znaków specjalnych i liczb.",
+      label: "Imię",
+      pattern: "^[A-Za-z]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Podaj swój adres e-mail",
+      errorMessage: "Wprowadź poprawy adres!",
+      label: "E-mail",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "date",
+      errorMessage:
+        "Data urodzenia nie jest konieczna, ale podając ją możemy lepiej dostosować treści jakie możesz od nas otrzymywać.",
+      label: "Data urodzenia",
+    },
+  ];
   return (
     <form>
       <h2>Zapisz się na webinar!</h2>
@@ -43,11 +43,16 @@ export default function Form() {
 }
 
 function FormInput({ input }) {
+  const [focused, setFocused] = useState(false);
   const { label, errorMessage } = input;
+
+  function handleFocus(e) {
+    setFocused(true);
+  }
   return (
     <div className="formInput">
       <label>{label}</label>
-      <input {...input} />
+      <input {...input} onBlur={handleFocus} focused={focused.toString()} />
       <span>{errorMessage}</span>
     </div>
   );
