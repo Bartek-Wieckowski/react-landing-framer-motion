543027d57783417a6aef070c747b7a27609fadc8
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-18 18:07:35 +0200

    removed animation, logic render button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,42 +21,17 @@ const containerVariants = {
   },
 };
 
-const buttonVariants = {
-  hover: {
-    scale: 1.1,
-    textShadow: "0px 0px 8px rgb(255,255,255)",
-    boxShadow: "0px 0px 8px rgb(255,255,255)",
-    transition: {
-      repeatType: "mirror",
-      repeat: Infinity,
-      duration: 0.3,
-    },
-  },
-};
-
 export default function Home() {
   const [countdownExpired, setCountdownExpired] = useState(false);
+
   function handleCountdownExpired() {
     setCountdownExpired(true);
   }
+
   return (
     <motion.div className="home" variants={containerVariants} initial="hidden" animate="visible" exit="exit">
       <div className="home-wrapper">
         <CountdownTimer targetDate={landingSettings.date} onCountdownExpired={handleCountdownExpired} />
-
-        {countdownExpired ? (
-          <a href="https://www.misjaperfekcja.pl/blog" target="_blank" rel="noopener noreferrer">
-            <motion.button className="button" variants={buttonVariants} whileHover="hover">
-              CZYTAJ NASZ BLOG!
-            </motion.button>
-          </a>
-        ) : (
-          <Link to="/about">
-            <motion.button className="button" variants={buttonVariants} whileHover="hover">
-              DOWIEDZ SIĘ WIĘCEJ...
-            </motion.button>
-          </Link>
-        )}
       </div>
     </motion.div>
   );
