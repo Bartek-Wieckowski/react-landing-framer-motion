71814f789c3d5dc592c538588c19ce004619764f
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-18 18:08:54 +0200

    added derived state and added logic if webinar is passed dont show info

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { motion } from "framer-motion";
-import imagePaths from "../utils/images"
+import imagePaths from "../utils/images";
 
 export default function Header({ webinarTitle, webinarDate }) {
-  const formattedDay = webinarDate.day.toString().padStart(2, '0');
-  const formattedMonth = webinarDate.month.toString().padStart(2, '0');
+  const formattedDay = webinarDate.day.toString().padStart(2, "0");
+  const formattedMonth = webinarDate.month.toString().padStart(2, "0");
   const formattedYear = webinarDate.year;
 
   const formattedDate = `${formattedDay}.${formattedMonth}.${formattedYear}`;
-  
+  const currentDate = new Date();
+  const webinarDateTime = new Date(webinarDate.year, webinarDate.month - 1, webinarDate.day);
+  const webinarHasPassed = currentDate > webinarDateTime;
+
   return (
     <section>
       <header>
@@ -18,10 +21,10 @@ export default function Header({ webinarTitle, webinarDate }) {
           animate={{ y: -10 }}
           transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
         >
-          <h1>
-            Webinar : {webinarTitle}
+          <h1 style={!webinarHasPassed ? {} : { borderBottom: "none" }}>
+            {!webinarHasPassed ? `Webinar : ${webinarTitle}` : ""}
           </h1>
-          <h2>Data : {formattedDate}</h2>
+          <h2>{!webinarHasPassed ? `Data : ${formattedDate}` : ""}</h2>
         </motion.div>
       </header>
     </section>
