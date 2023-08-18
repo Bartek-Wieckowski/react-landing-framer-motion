715fd25a1666ceda3213ebfdaf82379f25a78cf8
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-18 13:05:24 +0200

    passed two props to About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import { landingSettings, webinarContentObj } from "./utils/data-settings";
+import { landingSettings, webinarContentTop, webinarContentBottom } from "./utils/data-settings";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Registration from "./pages/Registration";
@@ -11,17 +11,19 @@ import PageNotFound from "./pages/PageNotFound";
 function App() {
   return (
     <div className="app">
-      <Header
-        webinarTitle={landingSettings.title}
-        webinarDate={landingSettings.date}
-      />
+      <Header webinarTitle={landingSettings.title} webinarDate={landingSettings.date} />
 
       <div className="container">
         <BrowserRouter>
           <AnimatePresence mode="wait">
             <Routes>
               <Route path="/" element={<Home />}></Route>
-              <Route path="/about" element={<About webinarContent={webinarContentObj} />}></Route>
+              <Route
+                path="/about"
+                element={
+                  <About webinarContentTop={webinarContentTop} webinarContentBottom={webinarContentBottom} />
+                }
+              ></Route>
               <Route path="/registration" element={<Registration />}></Route>
               <Route path="/thanks" element={<Thanks />}></Route>
               <Route path="*" element={<PageNotFound />} />
