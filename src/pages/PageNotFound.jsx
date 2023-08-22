729caf084dd94b853fcc5366a8feaeb5c640b98b
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-22 10:59:34 +0200

    created page not found, added modal window and button to redirect home page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,6 +1,36 @@
+import { useState, useEffect } from "react";
+import AnimatedButton from "../components/AnimatedButton";
+import { Link } from "react-router-dom";
+import Modal from "../components/Modal";
 
 export default function PageNotFound() {
+  const [modalConfig, setModalConfig] = useState(null);
+
+  useEffect(() => {
+    setModalConfig({
+      isOpen: true,
+      buttonText: "Zamknij",
+      contentText: "Przepraszamy taka strona nie istnieje...",
+    });
+  }, []); 
+
+  function handleCloseModal() {
+    setModalConfig(null);
+  }
+
   return (
-    <div>PageNotFound</div>
-  )
+    <div className="page-not-found-wrapper">
+      <Link to="/">
+        <AnimatedButton>Strona główna</AnimatedButton>
+      </Link>
+      {modalConfig && (
+        <Modal
+          isOpen={modalConfig.isOpen}
+          onClose={handleCloseModal}
+          buttonText={modalConfig.buttonText}
+          contentText={modalConfig.contentText}
+        />
+      )}
+    </div>
+  );
 }
