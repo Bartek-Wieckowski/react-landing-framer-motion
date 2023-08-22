09c2a1d12c3bff13973ecce108578ae9a6104060
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-22 12:29:08 +0200

    added img

--- src/utils/images.js
+++ src/utils/images.js
@@ -4,6 +4,13 @@ import laptopImg from "../assets/laptop.png";
 import phoneImg from "../assets/phone.png";
 import laptopPDF from "../assets/cut-pdf.jpg";
 import phonePDF from "../assets/cut-phone-pdf.jpg";
+import przedipo1 from "../assets/przedipo1.jpg";
+import przedipo2 from "../assets/przedipo2.jpg";
+import przedipo3 from "../assets/przedipo3.jpg";
+import przedipo4 from "../assets/przedipo4.jpg";
+import przedipo5 from "../assets/przedipo5.jpg";
+import przedipo6 from "../assets/przedipo6.jpg";
+import swipe from "../assets/swipe.svg";
 
 const imagePaths = {
   logo,
@@ -12,6 +19,8 @@ const imagePaths = {
   phoneImg,
   laptopPDF,
   phonePDF,
+  swipe,
+  imagesSlider: [przedipo1, przedipo2, przedipo3, przedipo4, przedipo5, przedipo6],
 };
 
 export default imagePaths;
