a2b7bc6f8991bd7a1d35168c4396c2d028d84160
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-24 16:01:35 +0200

    removed Header component if user go to not defined route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import { landingSettings} from "./utils/data-settings";
+import { landingSettings } from "./utils/data-settings";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Registration from "./pages/Registration";
@@ -9,21 +9,20 @@ import Thanks from "./pages/Thanks";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
+  const availablePaths = ["/", "/about", "/registration", "/thanks"];
+  const currentPath = window.location.pathname;
+  const isKnownRoute = availablePaths.includes(currentPath);
+
   return (
     <div className="app">
-      <Header webinarTitle={landingSettings.title} webinarDate={landingSettings.date} />
+      {isKnownRoute && <Header webinarTitle={landingSettings.title} webinarDate={landingSettings.date} />}
 
       <div className="container">
         <BrowserRouter>
           <AnimatePresence mode="wait">
             <Routes>
               <Route path="/" element={<Home />}></Route>
-              <Route
-                path="/about"
-                element={
-                  <About />
-                }
-              ></Route>
+              <Route path="/about" element={<About />}></Route>
               <Route path="/registration" element={<Registration />}></Route>
               <Route path="/thanks" element={<Thanks />}></Route>
               <Route path="*" element={<PageNotFound />} />
